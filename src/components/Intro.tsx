e785b438aa5b7d81f510e8d575aeee88351fd5e4
Jae <giffordjacob0@gmail.com>
2023-05-01 09:44:05 +0800

    fixed hydration theme mismatch error on svg

--- src/components/Intro.tsx
+++ src/components/Intro.tsx
@@ -1,11 +1,15 @@
-import React from 'react';
+import React, { useEffect, useState } from 'react';
 import Image from 'next/image';
 import ThemeToggle from './ThemeToggle';
 import { useTheme } from 'next-themes';
 
 function Intro() {
   const { systemTheme, theme, setTheme } = useTheme();
-
+  const [mounted, setMounted] = useState(false);
+  const currentTheme = theme === 'system' ? systemTheme : theme;
+  useEffect(() => {
+    setMounted(true);
+  }, []);
   return (
     <div className='w-screen h-screen flex flex-col justify-start items-center'>
       <div className='flex flex-col text-[6vw] text-center w-8/12'>
@@ -22,7 +26,7 @@ function Intro() {
             width='24'
           >
             <path
-              fill={theme === 'dark' ? '#FFFFFF' : '#000000'}
+              fill={mounted && currentTheme === 'dark' ? '#FFFFFF' : '#000000'}
               d='M180 936q-24 0-42-18t-18-42V276q0-24 18-42t42-18h279v60H180v600h600V597h60v279q0 24-18 42t-42 18H180Zm202-219-42-43 398-398H519v-60h321v321h-60V319L382 717Z'
             />
           </svg>
--- src/components/ThemeToggle.tsx
+++ src/components/ThemeToggle.tsx
@@ -4,10 +4,11 @@ import { useTheme } from 'next-themes';
 function ThemeToggle() {
   const { systemTheme, theme, setTheme } = useTheme();
   const currentTheme = theme === 'system' ? systemTheme : theme;
-
   return (
     <button
-      onClick={() => (theme == 'dark' ? setTheme('light') : setTheme('dark'))}
+      onClick={() =>
+        currentTheme == 'dark' ? setTheme('light') : setTheme('dark')
+      }
     >
       ToggleTheme
     </button>
