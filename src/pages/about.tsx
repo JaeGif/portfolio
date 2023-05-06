1398d7e339f1ccb6d7d8404468e51df48ebd73c3
Jae <giffordjacob0@gmail.com>
2023-05-06 20:28:41 +0800

    punchout changed to light and about text updated

--- src/pages/about.tsx
+++ src/pages/about.tsx
@@ -54,43 +54,45 @@ function about() {
           </span>
         </h1>
         <div className='flex flex-col gap-5'>
-          <div className='flex flex-col overflow-hidden min-w-[33%] gap-3'>
-            <h2 className='text-xl font-bold text-blue-400'>Who am I?</h2>
-            <div className='flex flex-col sm:flex-row'>
-              <p className='sm:max-w-[50%]'>
-                I&apos;m Jacob, a Full Stack Engineer. I learned web development
-                with the Odin Project, and Full Stack Open, from the University
-                of Helsinki. I&apos;m extremely passionate about web development
-                and driven to succeed and constantly improve my skills. Every
-                project I make challenges me in some new way and forces me to
-                grow, and I wouldn&apos;t have it any other way.
+          <div className='flex flex-col sm:flex-row'>
+            <span className='flex flex-col gap-3 w-fit'>
+              <h2 className='text-2xl font-bold text-blue-400 w-fit'>
+                Who am I?
+              </h2>
+              <p className='sm:w-[50vw] text-lg'>
+                I&apos;m Jacob, a Full Stack Engineer with a passion for
+                building beautiful user-centric applications. I&apos;m
+                constantly learning new technologies and finding interesting UI
+                designs and making them my own.
               </p>
-              <div className='h-fit w-fit max-w-[33%] relative border-2 dark:border-white dark:bg-gray-950 bg-white border-gray-950 rounded-3xl mt-10 mb-10 p-2 sm:p-4 shadow-lg'>
-                <div className='rounded-xl overflow-hidden border-[1px] border-gray-950 p-2'>
-                  <Image
-                    height={500}
-                    width={400}
-                    src={'/assets/images/punchout.png'}
-                    alt='picture of Jacob'
-                    aria-label='Picture of Jacob'
-                  />
-                </div>
+              <p className='text-lg'>
+                I learned web development with the Odin Project, and Full Stack
+                Open, from the University of Helsinki. I&apos;m extremely
+                passionate about web development and driven to succeed and
+                constantly improve my skills. Every project I make challenges me
+                in some new way and forces me to grow, and I wouldn&apos;t have
+                it any other way.
+              </p>
+            </span>
+            <div className='h-full w-full flex justify-center items-center'>
+              <div className='sm:max-w-[50vw] h-full'>
+                <Image
+                  height={500}
+                  width={500}
+                  src={'/assets/images/punchout-light.png'}
+                  alt='picture of Jacob'
+                  aria-label='Picture of Jacob'
+                />
               </div>
             </div>
           </div>
         </div>
-        <Image
-          height={100}
-          width={100}
-          src={'/assets/images/arrow.png'}
-          alt='arrow'
-          aria-label='arrow to next section'
-        />
-        <div className='relative flex flex-col overflow-hidden min-w-[33%] justify-center gap-3 mt-10 mb-10'>
-          <h2 className='text-xl font-bold text-blue-400'>
+
+        <div className='relative flex flex-col overflow-hidden min-w-[33%] justify-center gap-3 mb-10'>
+          <h2 className='text-2xl font-bold text-blue-400'>
             More than just a developer.
           </h2>
-          <p>
+          <p className='text-lg'>
             Outside of full stack development, I dabble in hobbies that help
             fuel my creativity. I take and edit photos with a Canon T6 Rebel,
             Photoshop and Lightroom. I fly a DJI Mavic Mini 2 drone for some
@@ -99,7 +101,7 @@ function about() {
             I&apos;ve taken recently.
           </p>
         </div>
-        <h2 className='text-xl font-bold text-blue-400'>
+        <h2 className='text-2xl font-bold text-blue-400'>
           Photography Showcase
         </h2>
         <ImageCarousel />
