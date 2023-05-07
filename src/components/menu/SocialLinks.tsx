2a0c8c525b33c5e47e12fb9c55ed1a44f88794db
Jae <giffordjacob0@gmail.com>
2023-05-07 17:27:53 +0800

    added links to gh and linkedin mobioe menu

--- src/components/menu/SocialLinks.tsx
+++ src/components/menu/SocialLinks.tsx
@@ -1,12 +1,24 @@
 import React from 'react';
 import Github from '../svg/Github';
 import LinkedIn from '../svg/LinkedIn';
-
+import Link from 'next/link';
 function SocialLinks() {
   return (
     <div className='flex gap-5'>
-      <Github />
-      <LinkedIn />
+      <Link
+        href='https://github.com/JaeGif'
+        aria-label='github'
+        target='_blank'
+      >
+        <Github />
+      </Link>
+      <Link
+        href='https://www.linkedin.com/in/jacob-gifford-dev/'
+        aria-label='linkedin'
+        target='_blank'
+      >
+        <LinkedIn />
+      </Link>
     </div>
   );
 }
