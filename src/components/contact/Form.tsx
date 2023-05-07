33d2dd3a341edc33191b2f622c2ba7e8eaad7b87
Jae <giffordjacob0@gmail.com>
2023-05-07 12:55:33 +0800

    form submit live

---
+++ src/components/contact/Form.tsx
@@ -0,0 +1,56 @@
+import React, { useState } from 'react';
+import { ThreeDots } from 'react-loading-icons';
+
+function Form() {
+  return (
+    <div className='relative border-2 dark:border-white border-gray-950 rounded-lg p-4 dark:bg-gray-800 bg-white'>
+      <div className='absolute top-5 left-5 h-full w-full dark:bg-white bg-gray-950 rounded-lg p-4 -z-10'></div>
+      <form
+        action='https://formsubmit.co/bddcd0d1879966d8248ec038d7fcd610'
+        method='POST'
+        className='min-w-[33vw] flex flex-col gap-3'
+      >
+        <div className='flex flex-col'>
+          <label htmlFor='name'>Name</label>
+          <input
+            className='h-10 p-2 rounded-md bg-gray-60 dark:bg-gray-700'
+            id='name'
+            required
+            type='text'
+            name='name'
+          />
+        </div>
+        <div className='flex flex-col'>
+          <label htmlFor='email'>Email</label>
+          <input
+            className='h-10 p-2 rounded-md bg-gray-60 dark:bg-gray-700'
+            id='email'
+            required
+            type='email'
+            name='email'
+          />
+        </div>
+        <div className='flex flex-col'>
+          <label htmlFor='message'>Message</label>
+          <textarea
+            className='resize-y h-24 min-h-[2.5rem] rounded-md p-2 bg-gray-60 dark:bg-gray-700'
+            id='message'
+            name='message'
+            required
+            maxLength={150}
+            minLength={1}
+          ></textarea>
+        </div>
+        <button
+          className='flex w-full justify-center'
+          onClick={() => setIsSubmitting(true)}
+          type='submit'
+        >
+          Send
+        </button>
+      </form>
+    </div>
+  );
+}
+
+export default Form;
--- src/pages/contact.tsx
+++ src/pages/contact.tsx
@@ -2,6 +2,7 @@ import React from 'react';
 import TransitionEffect from '@/components/menu/TransitionEffect';
 import Head from 'next/head';
 import { motion } from 'framer-motion';
+import Form from '@/components/contact/Form';
 function contact() {
   return (
     <>
@@ -17,7 +18,7 @@ function contact() {
             initial={{ y: 90, opacity: 0 }}
             animate={{ y: 0, opacity: 1 }}
             transition={{ delay: 1.2, duration: 0.5 }}
-            className='text-2xl sm:text-[4vw]'
+            className='text-3xl sm:text-[4vw]'
           >
             Let&apos;s
           </motion.p>
@@ -26,7 +27,7 @@ function contact() {
               initial={{ y: 90, opacity: 0 }}
               animate={{ y: 0, opacity: 1 }}
               transition={{ delay: 1.5, duration: 0.5 }}
-              className='text-2xl sm:text-[4vw]'
+              className='text-3xl sm:text-[4vw]'
             >
               Chat
             </motion.p>
@@ -34,50 +35,13 @@ function contact() {
               initial={{ y: 90, opacity: 0 }}
               animate={{ y: 0, opacity: 1 }}
               transition={{ delay: 1.8, duration: 0.5 }}
-              className='text-2xl sm:text-[4vw]'
+              className='text-3xl sm:text-[4vw]'
             >
               .
             </motion.p>
           </span>
         </span>
-        <div className='relative border-2 dark:border-white border-gray-950 rounded-lg p-4 dark:bg-gray-800 bg-white'>
-          <div className='absolute top-5 left-5 h-full w-full dark:bg-white bg-gray-950 rounded-lg p-4 -z-10'></div>
-          <form
-            action='https://formsubmit.co/88a570e5b09812d65851f42042cdbe95'
-            method='POST'
-            className='min-w-[33vw] flex flex-col gap-3'
-          >
-            <div className='flex flex-col'>
-              <label htmlFor='name'>Name</label>
-              <input
-                className='h-10 p-2 rounded-md bg-gray-60 dark:bg-gray-700'
-                id='name'
-                required
-                type='text'
-              />
-            </div>
-            <div className='flex flex-col'>
-              <label htmlFor='email'>Email</label>
-              <input
-                className='h-10 p-2 rounded-md bg-gray-60 dark:bg-gray-700'
-                id='email'
-                required
-                type='email'
-              />
-            </div>
-            <div className='flex flex-col'>
-              <label htmlFor='message'>Message</label>
-              <textarea
-                className='resize-y h-24 min-h-[2.5rem] rounded-md p-2 bg-gray-60 dark:bg-gray-700'
-                id='message'
-                required
-                maxLength={150}
-                minLength={1}
-              ></textarea>
-            </div>
-            <button type='submit'>Send</button>
-          </form>
-        </div>
+        <Form />
       </div>
     </>
   );
