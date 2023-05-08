4c844411a7acf950a88c2a818130f3b54777161c
Jae <giffordjacob0@gmail.com>
2023-05-08 08:10:32 +0800

    image carousel now works in mobile implementation

--- src/components/about/ImageCarousel.tsx
+++ src/components/about/ImageCarousel.tsx
@@ -1,4 +1,4 @@
-import React, { useState } from 'react';
+import React, { TouchEventHandler, useState } from 'react';
 import { motion } from 'framer-motion';
 function ImageCarousel() {
   const [mouseDownAt, setMouseDownAt] = useState<number>(0);
@@ -18,62 +18,93 @@ function ImageCarousel() {
       nextPercentage = Math.max(Math.min(nextPercentageNoBorder, 50), -50);
     setPercentageMoved(nextPercentage);
   };
-
+  const handleTouchStart = (e: React.TouchEvent<HTMLDivElement>) => {
+    setMouseDownAt(e.touches[0].clientX);
+  };
+  const handleTouchMove = (e: React.TouchEvent<HTMLDivElement>) => {
+    if (mouseDownAt === 0) return;
+    const mouseDelta = mouseDownAt - e.touches[0].clientX,
+      maxDelta = window.innerWidth / 2;
+    const percentage = (mouseDelta / maxDelta) * -100,
+      nextPercentageNoBorder = previousPercentageMoved + percentage,
+      nextPercentage = Math.max(Math.min(nextPercentageNoBorder, 50), -50);
+    setPercentageMoved(nextPercentage);
+  };
   return (
-    <motion.div
-      id='photography'
-      style={{
-        transform: `translate(${percentageMoved}%, 0%)`,
-      }}
-      onMouseLeave={() => {
-        setMouseDownAt(0);
-        setPreviousPercentageMoved(percentageMoved);
-      }}
-      onMouseUp={() => {
-        setMouseDownAt(0);
-        setPreviousPercentageMoved(percentageMoved);
-      }}
-      onMouseMove={(e) => handleMouseMove(e)}
-      onMouseDown={(e) => handleMouseDown(e)}
-      className={`h-80 flex gap-4 select-none justify-center`}
-    >
-      <motion.img
-        whileHover={{ scale: 1.3, y: -30 }}
-        animate={{ objectPosition: `${100 + percentageMoved}% center)` }}
-        draggable={false}
-        className={`w-[40vmin] h-full object-cover`}
-        src={'/assets/images/photography/beach.jpg'}
-        alt='photography showcase'
-        style={{ objectPosition: `${percentageMoved + 50}% center` }}
-      />
-      <motion.img
-        whileHover={{ scale: 1.3, y: -30 }}
-        animate={{ objectPosition: `${100 + percentageMoved}% center)` }}
-        draggable={false}
-        className={`w-[40vmin] h-full object-cover`}
-        src={'/assets/images/photography/mountains2.jpg'}
-        alt='photography showcase'
-        style={{ objectPosition: `${percentageMoved + 50}% center` }}
-      />
-      <motion.img
-        whileHover={{ scale: 1.3, y: -30 }}
-        animate={{ objectPosition: `${100 + percentageMoved}% center)` }}
-        draggable={false}
-        className={`w-[40vmin] h-full object-cover hidden sm:flex`}
-        src={'/assets/images/photography/swimming.jpg'}
-        alt='photography showcase'
-        style={{ objectPosition: `${percentageMoved + 50}%` }}
-      />
-      <motion.img
-        whileHover={{ scale: 1.3, y: -30 }}
-        animate={{ objectPosition: `${100 + percentageMoved}% center)` }}
-        draggable={false}
-        className={`w-[40vmin] h-full object-cover`}
-        src={'/assets/images/photography/shh.jpg'}
-        alt='photography showcase'
-        style={{ objectPosition: `${percentageMoved + 50}%` }}
-      />
-    </motion.div>
+    <div className='debug border-blue-600 w-full flex justify-center'>
+      <motion.div
+        id='photography'
+        style={{
+          transform: `translate(${percentageMoved}%, 0%)`,
+        }}
+        onMouseLeave={() => {
+          setMouseDownAt(0);
+          setPreviousPercentageMoved(percentageMoved);
+        }}
+        onMouseUp={() => {
+          setMouseDownAt(0);
+          setPreviousPercentageMoved(percentageMoved);
+        }}
+        onTouchStart={(e) => {
+          handleTouchStart(e);
+        }}
+        onTouchMove={(e) => {
+          handleTouchMove(e);
+        }}
+        onTouchEnd={(e) => {
+          setMouseDownAt(0);
+          setPreviousPercentageMoved(percentageMoved);
+        }}
+        onTouchCancel={(e) => {
+          setMouseDownAt(0);
+          setPreviousPercentageMoved(percentageMoved);
+        }}
+        onMouseMove={(e) => handleMouseMove(e)}
+        onMouseDown={(e) => handleMouseDown(e)}
+        className={`h-80 flex gap-4 select-none justify-center cursor-grab debug`}
+      >
+        <motion.img
+          whileHover={{ scale: 1.3, y: -30, zIndex: 10 }}
+          whileTap={{ scale: 1.3, y: -30, zIndex: 10 }}
+          animate={{ objectPosition: `${100 + percentageMoved}% center)` }}
+          draggable={false}
+          className={`w-[40vmin] h-full object-cover cursor-grab`}
+          src={'/assets/images/photography/beach.jpg'}
+          alt='photography showcase'
+          style={{ objectPosition: `${percentageMoved + 50}% center` }}
+        />
+        <motion.img
+          whileHover={{ scale: 1.3, y: -30, zIndex: 10 }}
+          whileTap={{ scale: 1.3, y: -30, zIndex: 10 }}
+          animate={{ objectPosition: `${100 + percentageMoved}% center)` }}
+          draggable={false}
+          className={`w-[40vmin] h-full object-cover cursor-grab`}
+          src={'/assets/images/photography/mountains2.jpg'}
+          alt='photography showcase'
+          style={{ objectPosition: `${percentageMoved + 50}% center` }}
+        />
+        <motion.img
+          whileHover={{ scale: 1.3, y: -30, zIndex: 10 }}
+          whileTap={{ scale: 1.3, y: -30, zIndex: 10 }}
+          animate={{ objectPosition: `${100 + percentageMoved}% center)` }}
+          draggable={false}
+          className={`w-[40vmin] h-full object-cover hidden sm:flex cursor-grab`}
+          src={'/assets/images/photography/swimming.jpg'}
+          alt='photography showcase'
+          style={{ objectPosition: `${percentageMoved + 50}%` }}
+        />
+        <motion.img
+          whileHover={{ scale: 1.3, y: -30, zIndex: 10 }}
+          whileTap={{ scale: 1.3, y: -30, zIndex: 10 }}
+          animate={{ objectPosition: `${100 + percentageMoved}% center)` }}
+          draggable={false}
+          className={`w-[40vmin] h-full object-cover cursor-grab`}
+          src={'/assets/images/photography/shh.jpg'}
+          alt='photography showcase'
+          style={{ objectPosition: `${percentageMoved + 50}%` }}
+        />
+      </motion.div>
+    </div>
   );
 }
 
--- src/components/about/ShowcaseHeader.tsx
+++ src/components/about/ShowcaseHeader.tsx
@@ -3,16 +3,19 @@ import ScrollArrowLeft from './ScrollArrowLeft';
 import ScrollArrowRight from './ScrollArrowRight';
 function ShowcaseHeader() {
   return (
-    <span className='flex gap-5 sm:gap-10 items-center justify-center w-full'>
-      <ScrollArrowLeft />
-      <h2 className='hidden sm:block text-2xl font-bold text-blue-400'>
-        Photography Showcase
-      </h2>
-      <h2 className='block sm:hidden text-2xl font-bold text-blue-400'>
-        Photos
-      </h2>
-      <ScrollArrowRight />
-    </span>
+    <div className='flex flex-col items-center'>
+      <span className='flex gap-5 sm:gap-10 items-center justify-center w-full'>
+        <ScrollArrowLeft />
+        <h2 className='hidden sm:block text-2xl font-bold text-blue-400'>
+          Photography Showcase
+        </h2>
+        <h2 className='block sm:hidden text-2xl font-bold text-blue-400'>
+          Photos
+        </h2>
+        <ScrollArrowRight />
+      </span>
+      <p className='text-sm text-gray-200'>Hint: grab and drag the photos</p>
+    </div>
   );
 }
 
