f377f5ba7824cf065c23a055861621edac8c07aa
Jae <giffordjacob0@gmail.com>
2024-09-19 21:56:20 -0700

    add 3

--- src/components/creative/Experiences/Scene.tsx
+++ src/components/creative/Experiences/Scene.tsx
@@ -6,10 +6,12 @@ import MarbleStand from './MarbleStand';
 import Island from '../projects/island/Island';
 import { useFrame, useThree } from '@react-three/fiber';
 import useExperienceStore from '@/components/stores/useExperienceStore';
+import * as THREE from 'three';
 
 interface TargetLocationsDict {
   [index: string]: { x: number; y: number; z: number };
 }
+
 const TARGET_LOCATIONS = {
   trophy: { x: 0, y: 0, z: 0 },
   terrain: { x: 1, y: 1, z: 1 },
@@ -34,6 +36,21 @@ function Scene({ cameraControls }: any) {
     // update state to look at new location
     if (selected) setTarget(TARGET_LOCATIONS[selected]);
   }, [selected]);
+  useEffect(() => {
+    if (target && cameraControls) {
+      const targetPos = new THREE.Vector3(target.x, target.y, target.z);
+      cameraControls.target.lerp(targetPos, 0.1);
+      cameraControls.update();
+    }
+  }, [target, cameraControls]);
+
+  useFrame((state, delta) => {
+    if (target && cameraControls) {
+      const targetPos = new THREE.Vector3(target.x, target.y, target.z);
+      cameraControls.target.lerp(targetPos, 0.01);
+      cameraControls.update();
+    }
+  });
 
   useEffect(() => {
     console.log(target);
