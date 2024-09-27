bd955884ed822b58dc1209778e5c6ca93d06a088
Jae <giffordjacob0@gmail.com>
2024-09-27 11:48:21 -0700

    update text template

--- src/components/creative/Experiences/texts/TextTemplate.tsx
+++ src/components/creative/Experiences/texts/TextTemplate.tsx
@@ -2,18 +2,14 @@ import React from 'react';
 import { Text3D } from '@react-three/drei';
 
 type TextTemplateProps = {
-  align?: 'left' | 'right' | 'center';
   scale?: number;
   lineHeight?: number;
-  maxWidth?: number;
   position?: [number, number, number];
   children: React.ReactNode;
 };
 function TextTemplate({
-  align = 'center',
   scale = 2,
   lineHeight = 1,
-  maxWidth = 10,
   position = [0, 12, 0],
   children,
 }: TextTemplateProps) {
@@ -22,9 +18,7 @@ function TextTemplate({
       castShadow
       font='/assets/fonts/pixeloidtf.json'
       scale={scale}
-      //   maxWidth={maxWidth}
       lineHeight={lineHeight}
-      //   textAlign={align}
       position={position}
     >
       {children}
