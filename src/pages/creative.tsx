e79326a54372069822e7a7a7d6b60abcefb079b7
Jae <giffordjacob0@gmail.com>
2024-09-30 13:14:41 -0700

    selection page looking good

--- src/pages/creative.tsx
+++ src/pages/creative.tsx
@@ -4,10 +4,16 @@ import TransitionEffect from '@/components/menu/TransitionEffect';
 import Experience from '@/components/creative/Experiences/Experience';
 import Creativity from '@/components/creative/Creativity';
 import ExperienceContainer from '@/components/creative/ExperienceContainer';
+import { motion } from 'framer-motion';
 
 function creative() {
   const [isExperience, setIsExperience] = useState(false);
   const [selected, setSelected] = useState(false);
+
+  const line = {
+    initial: { x: '-110%' },
+    animate: { x: '0' },
+  };
 
   return (
     <>
@@ -19,13 +25,57 @@ function creative() {
       <TransitionEffect />
       <section className='h-screen p-2 flex flex-col mt-8 gap-10'>
         {!selected ? (
-          <div className='h-full flex flex-col justify-around -mt-10 content-around'>
-            <h1 className='text-4xl w-full text-center'>
+          <div className='font-pixeloid h-full flex flex-col justify-around -mt-10 content-around'>
+            <h1 className='text-5xl w-full text-center'>
               Choose your adventure!
             </h1>
-            <div className='w-full flex justify-around items-center text-2xl'>
-              <button>Full Experience</button>
-              <button>Just the projects</button>
+            <div className='w-full flex justify-around items-center text-3xl'>
+              <div>
+                <motion.button
+                  initial='initial'
+                  animate='initial'
+                  whileHover='animate'
+                  className='overflow-hidden'
+                  onClick={() => {
+                    setSelected(true);
+                    setIsExperience(true);
+                  }}
+                >
+                  Full Experience
+                  <motion.div
+                    variants={line}
+                    className='dark:bg-white bg-gray-950 h-[2px] w-full'
+                  >
+                    <p className='invisible'>underline</p>
+                  </motion.div>
+                </motion.button>
+                <p className='text-sm text-blue-500'>
+                  Recommended for larger and more powerful devices
+                </p>
+              </div>
+              <div>
+                <motion.button
+                  initial='initial'
+                  animate='initial'
+                  whileHover='animate'
+                  className='overflow-hidden'
+                  onClick={() => {
+                    setSelected(true);
+                    setIsExperience(false);
+                  }}
+                >
+                  Just the projects
+                  <motion.div
+                    variants={line}
+                    className='dark:bg-white bg-gray-950 h-[2px] w-full'
+                  >
+                    <p className='invisible'>underline</p>
+                  </motion.div>
+                </motion.button>
+                <p className='text-sm text-blue-500'>
+                  Recommended for mobile devices or slow internet connections
+                </p>
+              </div>
             </div>
           </div>
         ) : (
