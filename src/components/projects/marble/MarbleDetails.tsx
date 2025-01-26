cb72e1517ba52412b2fe0a292f8a50ad4ee94e3f
Jae <giffordjacob0@gmail.com>
2025-01-25 20:49:54 -0800

    add marble run img

--- src/components/projects/marble/MarbleDetails.tsx
+++ src/components/projects/marble/MarbleDetails.tsx
@@ -15,7 +15,7 @@ function MarbleDetails() {
   return (
     <span className='flex w-full p-2 sm:p-5 gap-5'>
       <h1 className='text-7xl hidden sm:flex w-20'>1.</h1>
-      <div className='flex flex-col sm:flex-row relative sm:w-5/6 shadow-md border-2 dark:border-white border-gray-950 bg-white dark:bg-gray-800 rounded-md mt-5 p-4 items-center'>
+      <div className='flex flex-col lg:flex-row relative sm:w-5/6 shadow-md border-2 dark:border-white border-gray-950 bg-white dark:bg-gray-800 rounded-md mt-5 p-4 items-center'>
         <div className='hidden sm:flex absolute top-3 left-3 h-full w-full bg-gray-950 dark:bg-white rounded-xl -z-10'></div>
         <div className='flex flex-col'>
           <div>
@@ -61,24 +61,23 @@ function MarbleDetails() {
             </div>
           </div>
         </div>
-        <div className='flex gap-2 justify-center'>
+        <div className='flex gap-2 justify-center items-center'>
           <motion.div
             whileHover={{
               x: isLg ? -100 : 0,
-              scale: isLg ? 1.5 : 1.1,
+              scale: isLg ? 1.2 : 1.1,
               transition: { duration: 0.2 },
             }}
             whileTap={{
               x: isLg ? -100 : 0,
-
-              scale: isLg ? 1.5 : 1.1,
+              scale: isLg ? 1.2 : 1.1,
               transition: { duration: 0.2 },
             }}
-            className='rounded-xl overflow-hidden shadow-lg h-96'
+            className='rounded-xl overflow-hidden shadow-lg'
           >
             <img
-              className='h-full max-w-fit'
-              src='/assets/images/gradient/mobile.png'
+              className='max-w-full object-contain'
+              src='/assets/images/marble/desktop.png'
               alt='project image'
               aria-label='project mobile view'
             />
